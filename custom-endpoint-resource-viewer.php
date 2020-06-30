eac82742c845d91a5b438413057d642f9700b2bf
balfonso <maximuz313@gmail.com>
2020-06-30 23:47:13 +0800

    Setup main plugin file; Add basic plugin dir structure

---
+++ custom-endpoint-resource-viewer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package  CERV
+ */
+/*
+Plugin Name: Custom Endpoint Resource Viewer
+Plugin URI: https://github.com/byronalfonso
+Author: Byron Alfonso
+Author URI: https://github.com/byronalfonso
+Description: CERV generates a custom endpoint that allows a user to view a resource from an external API on that endpoint.
+Text Domain: custom-endpoint-resource-viewer
+Version: 1.0.0
+*/
+
+defined( 'ABSPATH' ) || exit;
