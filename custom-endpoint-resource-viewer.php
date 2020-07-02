81834a23fcc03ab663e7c4a843d254bcfa06b99c
balfonso <maximuz313@gmail.com>
2020-07-02 16:30:40 +0800

    Add the functionality to add a rewrite tag and rule for custom endpoint; Add functionality to override the template for the custom endpoint; Prepare basic template for the resource listing

--- custom-endpoint-resource-viewer.php
+++ custom-endpoint-resource-viewer.php
@@ -14,11 +14,48 @@ Version: 1.0.0
 
 defined( 'ABSPATH' ) || exit;
 
+// echo();
+// die;
+
+
 // Enable autoloading
 if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
 
-use Includes\TestClass;
-$testClass = new TestClass();
+function cerv_activate_plugin() {
+	flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'cerv_activate_plugin' );
+
+/**
+ * The code that runs during plugin deactivation
+ */
+function cerv_deactivate_plugin() {
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'cerv_deactivate_plugin' );
+
+function custom_enpoint_rewrite_rule() {	
+	add_rewrite_rule('^cerv$', 'index.php?cerv=1', 'top');
+	flush_rewrite_rules();  
+}
+add_action('init', 'custom_enpoint_rewrite_rule');
+
+function custom_rewrite_tag( $vars ) {
+	add_rewrite_tag( '%cerv%', '([^&]+)');
+}
+add_action( 'init', 'custom_rewrite_tag');
+
+function override_custom_enpoint_template( $original_template ) {
+	$customEndpoint = get_query_var( 'cerv' );
+	
+	if ( $customEndpoint ) {
+		$template = plugin_dir_path( __FILE__ ) . 'templates/custom.php';
+		return $template;
+	}
+	
+	return $original_template;
+}
+add_filter( 'template_include', 'override_custom_enpoint_template' );
---
+++ templates/custom.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "<h1>CUSTOM BABY!</h1>";
