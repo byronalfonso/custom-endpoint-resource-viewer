756d1167f84caf33581bb7b95fb4616a9300fbd4
balfonso <maximuz313@gmail.com>
2020-07-07 21:32:40 +0800

    Add loader functionality when fetching resource details; Complete the requesting and processing of resource details; Add error handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,100 @@
 ( function( $ ) {
 	$( document ).ready( function() {
-        /* Cache the modal */
+        // Cache the modal and loader
         $modal = $('.cerv-modal-overlay');
+        $loader = $('.cerv-loader-container');
 
         $('.user-details-link').on('click', function(e){
             e.preventDefault();
+            runModal();
             let userId = $(this).data('user-id');
-            $modal.show();
+            let userDetailsPath = `${cervObj.api_endpoint}/users/${userId}`;
+            getResourceDetails( userDetailsPath );
+        });
 
-            // jQuery.ajax({
-            //     type:"POST",
-            //     url: esd_ajax_obj.ajax_url,
-            //     data: {
-            //     action: "esd_fetch_skills"
-            //     },
-            //     success:function(response){
-            //     loadExamResults(response.data.skills);
-            //     },
-            //     error: function(errorThrown){
-            //     alert('Unable to process request: ' + errorThrown);
-            //     console.log(response, 'response');
-            //     }
-            // });
-
-        })
-
-        /* Hide the modal window if overlay is clicked */
+        // Hide the modal window if overlay is clicked
         $modal.on('click', function(e){
-            $modal.hide();
-        })
+            terminateModal();
+        });
 
-        /* Hide the modal window if x button is clicked */
+        // Hide the modal window if x button is clicked
         $('.close-cerv-modal-btn').on('click', function(e){
-            $modal.hide();
-        })
+            terminateModal();
+        });
+
+
+        // Display the loader and modal
+        function runModal(){
+            $loader.show();
+            $modal.show();            
+        }
+
+        // Hide the loader and modal
+        function terminateModal(){
+            $loader.hide();
+            $modal.hide();            
+        }
+
+        // processes the API request for resource details
+        function getResourceDetails($url){
+            _GET($url, populateResourceDetails, displayError);
+        }
+        
+        // If request is successful, populate the modal box with dynamic data
+        function populateResourceDetails(data){
+            let address = data.address;
+            let company = data.company;
+
+            let content = (
+                `<div class="user-info">
+                    <h5>User Info</h5>
+                    <p><span class="modal-label">Username: </span> ${data.username}</p>
+                    <p><span class="modal-label">Email: </span> ${data.email}</p>
+                    <p><span class="modal-label">Phone: </span> ${data.phone}</p>
+                    <p><span class="modal-label">Website: </span> ${data.website}</p>
+                    <p><span class="modal-label">Address: </span> ${address.suite}, ${address.street}, ${address.city}, ${address.zipcode}</p>
+                </div>
+                <hr />
+                <div class="company">
+                    <h5>Company</h5>
+                    <p><span class="modal-label">Name: </span>${company.name}</p>
+                    <p><span class="modal-label">Catch Phrase: </span>${company.catchPhrase}</p>
+                    <p><span class="modal-label">BS: </span>${company.bs}</p>
+                </div>`
+            );
+            
+            $('.cerv-modal-header .title').text(data.name);
+            $('.cerv-modal-content').html(content);
+            $loader.hide();
+        }
+        
+        // If request failed, make sure to display an error to the user
+        function displayError(jqXHR, textStatus, error){
+            $('.cerv-modal-header .title').text('Error');
+
+            let content = (
+                `<div class="error">
+                    <p>Unable to process your request.</p>
+                    <p>Please reload your page or try again later.</p>
+                </div>`
+            );
+
+            $('.cerv-modal-content').html(content);
+            $loader.hide();
+        }
+        
+        // Encapsulate an ajax GET request
+        function _GET(url, successCB, failCB){
+            $.ajax({
+                type:"GET",
+                url: url,
+                success:function(resp){
+                    successCB(resp)
+                },
+                error: function(jqXHR, textStatus, error){
+                    failCB(jqXHR, textStatus, error);
+                }
+            });
+        }
     });
 }( jQuery ) );
--- templates/partials/modal.php
+++ templates/partials/modal.php
@@ -1,26 +1,31 @@
-<div class="cerv-modal-overlay">
+<div class="cerv-modal-overlay">    
     <div class="cerv-modal">
+        <span class="close-cerv-modal-btn">&times;</span>
+        <div class="cerv-loader-container">
+            <div class="loader"></div>
+            <h5>Loading Resources...</h5>
+        </div>
         <div class="cerv-modal-header">
-            <h4 class="name">Leanne Graham</h4>
-            <span class="close-cerv-modal-btn">&times;</span>
+            <h4 class="title"></h4>
         </div>
         <div class="cerv-modal-content">
-            <div class="user-info">
-                <h5>User Info</h5>
-                <p><span class="modal-label">Username: </span> Bret</p>
-                <p><span class="modal-label">Email: </span> Sincere@april.biz</p>
-                <p><span class="modal-label">Phone: </span> 1-770-736-8031 x56442</p>
-                <p><span class="modal-label">Website: </span> hildegard.org</p>
-                <p><span class="modal-label">Address: </span> Apt. 556, Kulas Light, Gwenborough, 92998-3874</p>
-            </div>
-            <hr />
-            <div class="company">
-                <h5>Company</h5>
-                <p><span class="modal-label">Name: </span>Romaguera-Crona</p>
-                <p><span class="modal-label">Catch Phrase: </span>Multi-layered client-server neural-net</p>
-                <p><span class="modal-label">BS: </span>harness real-time e-markets</p>
-            </div>  
+            
         </div>
     </div>
 </div>
 
+<!-- <div class="user-info">
+    <h5>User Info</h5>
+    <p><span class="modal-label">Username: </span> Bret</p>
+    <p><span class="modal-label">Email: </span> Sincere@april.biz</p>
+    <p><span class="modal-label">Phone: </span> 1-770-736-8031 x56442</p>
+    <p><span class="modal-label">Website: </span> hildegard.org</p>
+    <p><span class="modal-label">Address: </span> Apt. 556, Kulas Light, Gwenborough, 92998-3874</p>
+</div>
+<hr />
+<div class="company">
+    <h5>Company</h5>
+    <p><span class="modal-label">Name: </span>Romaguera-Crona</p>
+    <p><span class="modal-label">Catch Phrase: </span>Multi-layered client-server neural-net</p>
+    <p><span class="modal-label">BS: </span>harness real-time e-markets</p>
+</div>   -->
