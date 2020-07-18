902c1868780da4bca172bd9e7a9b4064acc5a48b
balfonso <maximuz313@gmail.com>
2020-07-19 00:50:32 +0800

    Improve activation and deactivation; Add uninstall.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,8 @@
 <?php
+
+declare(strict_types=1);
+
 // Enable autoloading
 if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
-
-// Setup activation hook
-function cerv_activate_plugin() {
-	flush_rewrite_rules();
-}
-register_activation_hook( __FILE__, 'cerv_activate_plugin' );
-
-// Setup deactivation hook
-function cerv_deactivate_plugin() {
-	flush_rewrite_rules();
-}
-register_deactivation_hook( __FILE__, 'cerv_deactivate_plugin' );
