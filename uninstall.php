60263fd1c2589c830fd9dad2d1aacac8f87ac6dd
balfonso <maximuz313@gmail.com>
2020-07-20 00:11:50 +0800

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 if (!defined('WP_UNINSTALL_PLUGIN')) die;
 
-$settingOptions = ['resource_select'];
+$settingOptions = ['resource_select', 'cerv_custom_endpoint_field'];
  
 // Clean up Settings
 foreach ( $settingOptions as $option ) {
