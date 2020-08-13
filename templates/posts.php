26581f21943d8ee855c829d3646846bbdafd5460
balfonso <maximuz313@gmail.com>
2020-08-13 20:34:29 +0800

    Fix responsive design columns for the posts resource and separate it with the users

--- templates/posts.php
+++ templates/posts.php
@@ -2,6 +2,15 @@
 
 <?php if ( !empty($templateResource) ) : ?>
 
+<style>
+    @media only screen 
+    and (max-width: 760px), (min-device-width: 768px) 
+    and (max-device-width: 1024px)  {
+        table.cerv-resource-table td:nth-of-type(1):before { content: "Post ID"; }
+        table.cerv-resource-table td:nth-of-type(2):before { content: "Title"; }
+    }
+</style>
+
 <div class="cerv-container">
     <h2 class="cerv-resource-title"><?php echo $templateResource['title']?></h2>
     <?php include_once  plugin_dir_path( __FILE__ ) . 'partials/modal.php'; ?>
--- templates/users.php
+++ templates/users.php
@@ -2,6 +2,19 @@
 
 <?php if ( !empty($templateResource) ) : ?>
 
+<style>
+    @media only screen 
+    and (max-width: 760px), (min-device-width: 768px) 
+    and (max-device-width: 1024px)  {
+        table.cerv-resource-table td:nth-of-type(1):before { content: "User ID"; }
+        table.cerv-resource-table td:nth-of-type(2):before { content: "Username"; }
+        table.cerv-resource-table td:nth-of-type(3):before { content: "Name"; }
+        table.cerv-resource-table td:nth-of-type(4):before { content: "Email"; }
+        table.cerv-resource-table td:nth-of-type(5):before { content: "Website"; }
+        table.cerv-resource-table td:nth-of-type(6):before { content: "Company"; }
+    }
+</style>
+
 <div class="cerv-container">
     <h2 class="cerv-resource-title"><?php echo $templateResource['title']?></h2>
     <?php include_once  plugin_dir_path( __FILE__ ) . 'partials/modal.php'; ?>
